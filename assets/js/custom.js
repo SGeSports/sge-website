e7a41173770a7bab483e87641e832a397b384d81
Connor <connor.postma@gmail.com>
2020-08-30 21:18:24 -0400

    removed poptrox code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,39 +7,5 @@
 		});  	
 		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
 	}); 
-
-	// var foo = $('#gallery');
-	// foo.poptrox({
-	// 	usePopupCaption: true
-	// });
-
-	$(function() {
-		var $window = $(window),
-			$body = $('body');
-
-		// Poptrox.
-		$window.on('load', function() {
-
-			var $thumbs = $('#gallery');
-			//var $thumbs = $('.portfolio');
-
-			$thumbs.poptrox();
-			// if ($thumbs.length > 0)
-			// 	$thumbs.poptrox({
-			// 		//onPopupClose: function() { $body.removeClass('is-covered'); },
-			// 		//onPopupOpen: function() { $body.addClass('is-covered'); },
-			// 		baseZIndex: 10001,
-			// 		useBodyOverflow: false,
-			// 		overlayColor: '#222226',
-			// 		overlayOpacity: 0.75,
-			// 		popupLoaderText: '',
-			// 		fadeSpeed: 500,
-			// 		usePopupDefaultStyling: false,
-			// 		windowMargin: 5
-			// 	});
-
-		});
-	});
-
 		
 })(jQuery);
